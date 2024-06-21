c68cef3f88a0e6d9037af4cc147d1d817db432d6
テイイ <148879729+Tetsu45@users.noreply.github.com>
2024-06-21 14:41:10 +0900

    Update A.cpp-1

--- A.cpp
+++ A.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 using namespace std;
+int* read_inputs(int* size);
 int main(){
-  return 0;
+    int n;int q;int count = 0;
+    cin >> n;
+    int* arr1 = read_inputs(&n);
+    cin >> q;
+    int* arr2 =  read_inputs(&q);
+    for (int  j = 0; j < q; j++ ){
+        for(int i =0; i <n; i++){
+            if(arr1[i] == arr2[j]){
+                count++;
+                break;
+            }
+        }
+    }
+    cout << count << endl;
+    delete[] arr1;
+    delete[] arr2;
+    return 0;
 }
-  
-  
+int* read_inputs(int* size){
+    int* arr =  new int[*size];
+    for(int i = 0; i< *size; i++){
+        std::cin >> arr[i];
+    }
+    return arr;
+}
